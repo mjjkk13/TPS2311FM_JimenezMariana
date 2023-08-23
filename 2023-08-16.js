87100cc35c30c130a51a1076f9cfba36e7154392
CamiloPinzon <camilopinzon2018@gmail.com>
2023-08-23 08:49:12 -0500

    act

---
+++ 2023-08-16.js
@@ -0,0 +1,64 @@
+function DigameAsi(name, nickname) { 
+    console.log("Mi nombre es" + name + ",pero prefiero que me digas" + nickname + ".") 
+}
+
+DigameAsi("Mariana","Mariane               ")
+
+let nombre = 'Mariana'
+let apellido = 'Jiménez Villa'
+let username = 'Mariane'
+let edad = '17'
+let email = 'marianajimenezv2006@gmail.com'
+let EsMayorDeEdad = 'false'
+let dineroAhorrrado = '300'
+let deudas = '20'
+
+/* NombreCompleto (nombre y apellido) */
+ NombreCompleto = nombre + apellido 
+/*Dinero real (dinero ahorrado menos deudas) */
+ DineroReal = dineroAhorrrado - deudas
+
+ for (let i = 0; i < 5; i++) {
+    console.log("El valor i es:" + i); 
+}
+
+
+let i = 0
+
+while (i < 5) {
+    console.log("El valor i es:" + i)
+    i++;
+}
+
+let respuesta
+
+while (respuesta != '4') {
+    let pregunta = prompt('¿Cuanto es 2 + 2?')
+    respuesta = pregunta ;
+}
+
+/* Como hacer una pregunta en un cartel, hasta que la respuesta sea valida
+no dejara de reptir el ciclo */
+
+function imprimirPrimerElementoArray(arr) {
+    console.log(arr[0])   
+}
+
+function imprimirPrimerElementoArray(arr) {
+    for (let i = 0; i <= arr.length; i++) 
+    console.log("El nombre del usuario es:" + arr[i]);
+}
+
+
+const obj = {
+    Nombre:'Fulanito',
+    edad: 3,
+    Comida: ['Pollo frito', 'vegetales']
+}
+
+function imprimirPrimerElementoArray(obj) {
+    const arr= object.values(obj);
+    for (let i = 0; i <= arr.length; i++) {
+    console.log("El nombre del usuario es:" + arr[i]); 
+    }
+}
