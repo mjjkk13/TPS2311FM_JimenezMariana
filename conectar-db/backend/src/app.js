f1e78b38c06c4d4ee15b5d86a913a9670d1a7612
mjjkk13 <mjimenez3871@misena.edu.co>
2023-09-18 09:59:39 -0500

    destructuring en js parte 2

--- conectar-db/backend/src/app.js
+++ conectar-db/backend/src/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const app = express();
 
-const juan = {
-    apodo: 'juanito',
-    edad: 29,
-};
+//const juan = {
+   // apodo: 'juanito',
+    //edad: 29,
+//};
+
+//const apodo = juan.apodo;
+//const edad = juan.edad;
+
+//const {apodo, edad} = juan;
+
 
 app.get('/:id', (req, res)=>{
     const {id}= req.params
-    res.send('Peticion GET');
+    res.send(`Peticion GET ${id}`);
 });
 
 app.post('/', (req, res)=>{
