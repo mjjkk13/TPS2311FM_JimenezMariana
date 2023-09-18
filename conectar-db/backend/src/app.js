cdbd07419a0cfee4da62e22b301912e0fcb3269d
mjjkk13 <mjimenez3871@misena.edu.co>
2023-09-18 09:36:15 -0500

    configuración métodos de peticiones http

--- conectar-db/backend/src/app.js
+++ conectar-db/backend/src/app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hola Mundo');
+    res.send('Peticion GET');
 });
+
+app.post('/', (req, res)=>{
+    res.send('Peticion POST');
+})
 
 app.listen(3000, ()=>{
     console.log('Servidor encendido');
