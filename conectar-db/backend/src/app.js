9680df3af830530ffb0c54d91d18b72a13898f2d
mjjkk13 <mjimenez3871@misena.edu.co>
2023-09-20 09:02:29 -0500

    pasar información por el body

--- conectar-db/backend/src/app.js
+++ conectar-db/backend/src/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/:name/:id', (req, res) =>{
     const {name,id} = req.params;
     res.send(` ${name}: ${id}`);
 });
 
 app.post('/', (req, res)=>{
-    res.send('Peticion POST');
+    const {email, password} = req.body;
+    res.send(`${email}: ${password}`);
 });
 
 app.put('/', (req, res)=>{
