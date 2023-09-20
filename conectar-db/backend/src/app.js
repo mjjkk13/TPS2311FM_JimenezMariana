9c9ff20cdaf52d9287eca0cbd2c7bc3c75d41845
mjjkk13 <mjimenez3871@misena.edu.co>
2023-09-20 10:09:05 -0500

    ejecutando querys a mysql desde el servidor

--- conectar-db/backend/src/app.js
+++ conectar-db/backend/src/app.js
@@ -38,6 +38,27 @@ connection.connect((err)=>{
     console.log('DB conectada');
 });
 
+//const querySQL = 'SHOW TABLES';
+
+//connection.query(querySQL, (err, res)=>{
+//    if (err) throw err;
+//    console.log('respuesta sql', res);
+//});
+
+const insertQuery = `INSERT INTO User(first_name,age) VALUES('Juan', 30)`;
+
+connection.query(insertQuery, (err, res)=> {
+    if(err) throw err;
+    console.log('respuesta insert', res);
+});
+
+const getQuery = `SELECT * FROM User;`;
+
+connection.query(getQuery, (err, res)=>{
+    if(err) throw err;
+    console.log('respuesta get', res);
+});
+
 app.listen(3000, ()=>{
     console.log('Servidor encendido');
 });
