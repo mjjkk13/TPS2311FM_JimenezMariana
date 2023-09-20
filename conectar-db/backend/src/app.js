0d05f1f404b5bbddc14e246ab26f631dbbfc0fa3
mjjkk13 <mjimenez3871@misena.edu.co>
2023-09-20 08:42:33 -0500

    creando primer ruta/endpoint

--- conectar-db/backend/src/app.js
+++ conectar-db/backend/src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) =>{
+    res.send('Hola mundo');
+});
+
 app.listen(3000, ()=>{
     console.log('Servidor encendido');
 });
